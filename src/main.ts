7cea72a0ecc4a103f6af9b98e89546438ab3cb03
Prakash <prakashsekar772@gmail.com>
2025-05-02 08:52:52 +0530

    cors changes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://ai-content-ext.onrender.com/'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
