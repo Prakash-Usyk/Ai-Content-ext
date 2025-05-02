4b036788e742702eee4cbdd56ebc621bb25808c5
Prakash <prakashsekar772@gmail.com>
2025-05-02 11:00:14 +0530

    cors changes

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://ai-content-ext-client.vercel.app/'],
+    origin: ['https://ai-content-ext-client.vercel.app'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
